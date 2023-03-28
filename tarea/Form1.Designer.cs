d56b470498065a47beae59b17e0642b2de8d792e
marlo <marlo@DESKTOP-T3BGM44>
2023-03-27 22:32:55 -0600

    Agregar archivos de proyecto.

---
+++ tarea/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace tarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.valor1 = new System.Windows.Forms.TextBox();
+            this.cmbSeleccionar = new System.Windows.Forms.ComboBox();
+            this.valor2 = new System.Windows.Forms.TextBox();
+            this.btnResolver = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(347, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(256, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(259, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "valor2";
+            // 
+            // valor1
+            // 
+            this.valor1.Location = new System.Drawing.Point(340, 118);
+            this.valor1.Name = "valor1";
+            this.valor1.Size = new System.Drawing.Size(100, 20);
+            this.valor1.TabIndex = 3;
+            // 
+            // cmbSeleccionar
+            // 
+            this.cmbSeleccionar.FormattingEnabled = true;
+            this.cmbSeleccionar.Items.AddRange(new object[] {
+            "Sumar",
+            "Restar",
+            "Multiplicar",
+            "Dividir"});
+            this.cmbSeleccionar.Location = new System.Drawing.Point(330, 234);
+            this.cmbSeleccionar.Name = "cmbSeleccionar";
+            this.cmbSeleccionar.Size = new System.Drawing.Size(121, 21);
+            this.cmbSeleccionar.TabIndex = 4;
+            // 
+            // valor2
+            // 
+            this.valor2.Location = new System.Drawing.Point(340, 177);
+            this.valor2.Name = "valor2";
+            this.valor2.Size = new System.Drawing.Size(100, 20);
+            this.valor2.TabIndex = 5;
+            // 
+            // btnResolver
+            // 
+            this.btnResolver.Location = new System.Drawing.Point(351, 356);
+            this.btnResolver.Name = "btnResolver";
+            this.btnResolver.Size = new System.Drawing.Size(75, 23);
+            this.btnResolver.TabIndex = 6;
+            this.btnResolver.Text = "Resolver";
+            this.btnResolver.UseVisualStyleBackColor = true;
+            this.btnResolver.Click += new System.EventHandler(this.btnResolver_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(340, 291);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnResolver);
+            this.Controls.Add(this.valor2);
+            this.Controls.Add(this.cmbSeleccionar);
+            this.Controls.Add(this.valor1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox valor1;
+        private System.Windows.Forms.ComboBox cmbSeleccionar;
+        private System.Windows.Forms.TextBox valor2;
+        private System.Windows.Forms.Button btnResolver;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ tarea/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tarea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResolver_Click(object sender, EventArgs e)
+        {
+            int v1 = Convert.ToInt32(valor1.Text);
+            int v2 = Convert.ToInt32(valor2.Text);
+            int respuesta;
+
+            if(cmbSeleccionar.SelectedIndex == -1)
+            {
+                MessageBox.Show("no has seleccionado nada");
+            }
+            else
+            {
+                switch (cmbSeleccionar.SelectedItem.ToString())
+                {
+                    case "Sumar":
+                        respuesta = v1 + v2;
+                        textBox1.Text = respuesta.ToString();
+                        break;
+
+                    case "Restar":
+                        respuesta = v1 - v2;
+                        textBox1.Text = respuesta.ToString();
+                        break;
+
+                    case "Multiplicar":
+                        respuesta = v1 * v2;
+                        textBox1.Text = respuesta.ToString();
+                        break;
+
+                    case "Dsividir":
+                        respuesta = v1 / v2;
+                        textBox1.Text = respuesta.ToString();
+                        break;
+
+                    default:
+                        MessageBox.Show("no se reconoce el valor ingresado");
+                        break;
+                }
+            }
+        }
+    }
+}
